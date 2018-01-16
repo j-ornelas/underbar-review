0cfa2a1bf7944f84c02e54af4f7df8407decbac1
j-ornelas <johnnyoh1988@gmail.com>
2018-01-16 11:16:39 -0800

    now works up until map

--- src/underbar.js
+++ src/underbar.js
@@ -80,16 +80,57 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var output = [];
+    _.each(collection, function(item) {
+      if (test(item)) {
+        output.push(item);
+      }
+    });
+    return output;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    return _.filter(collection, function(item) {
+      return test(item) === false;
+    });
   };
+
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array, isSorted, iterator) {
+    var output = [];
+    var truthys = [];
+    if (isSorted === true) {
+      _.each(array, function(item) {
+        truthys.push(iterator(item));
+      });
+      var f = false;
+      var t = false;
+      while (f === false && t === false) {
+        for (var i = 0; i < truthys.length; i++) {
+          if (f && t) {
+            break;
+          }
+          if (truthys[i] === true) {
+            output.push(array[i]);
+            t = true;
+          } else {
+            output.push(array[i]);
+            f = true;
+          }
+        }
+      }   
+    } else {
+      _.each(array, function(item) {
+        if (_.indexOf(output, item) === -1) {
+          output.push(item);
+        }
+      });
+    }
+    return output;
   };
 
 
@@ -98,6 +139,11 @@
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var output = [];
+    _.each(collection, function(item) {
+      output.push(iterator(item));
+    });
+    return output;
   };
 
   /*
